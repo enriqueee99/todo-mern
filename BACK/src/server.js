07181e908007994dadcfa645d50de053711c272b
Anthony Enrique <enriqueee99@gmail.com>
2026-01-22 11:43:43 -0500

    agregado a cors nuevo origen de netlify

--- BACK/src/server.js
+++ BACK/src/server.js
@@ -10,7 +10,7 @@ const app = express();
 //permite leer archivos json y transformarlos en formato object
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://todo-mern-note.netlify.app/"],
   }),
 );
 app.use(express.json());
